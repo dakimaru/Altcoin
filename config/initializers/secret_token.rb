f47c929f462563c1c2500591881d3b6c4f41ba2a
Dimitri Akimaru <akimaru@umich.edu>
2014-03-06 01:21:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Altcoin::Application.config.secret_token = '36ea588ee8e6ef5538d1061a3c8e1fb7b712395f6d0f53e8415d2246c9861b5861ffc0cb0c3703528c1702fb6234678c3562b241a6e173d49554752fd6ac3fda'

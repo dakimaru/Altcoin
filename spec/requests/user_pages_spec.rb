6fd03bf249ac8e82bb649482dda749f81f2e1bc7
Dimitri Akimaru <akimaru@umich.edu>
2014-03-13 16:38:51 -0400

    finished signup path creation with tests

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "User Pages" do
+	
+  subject {page}
+
+  describe "User Signup" do
+  	before {visit signup_path}
+  	it {should have_selector('h1', text: 'Sign up') }
+  	it {should have_title(full_title('Signup'))}
+  end
+end
